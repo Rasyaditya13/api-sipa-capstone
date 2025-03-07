8d59dde0a62298974e30c9e01f280bc2342aa382
asus <rasyadelgiva@gmail.com>
2025-03-07 14:34:03 +0700

    hhe

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import router from "./routes/web.js";
-import db from "./config/database.js";
 
 dotenv.config();
 
@@ -12,7 +11,7 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.use("/api", router);
+app.use(router);
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
