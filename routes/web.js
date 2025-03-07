659c3e8e973e5d72174baabf0670b20d6a4e36db
asus <rasyadelgiva@gmail.com>
2025-03-07 14:26:46 +0700

    Tes code

--- routes/web.js
+++ routes/web.js
@@ -1,17 +1,8 @@
 import express from "express";
-
 const router = express.Router();
 
-import {
-  getUser,
-  getUserById,
-  createUser,
-  updateUser,
-  deleteUser,
-} from "../controllers/userController.js";
+import {getUser} from "../controllers/userController.js";
 
 router.get("/users", getUser);
-router.get("/users/:id", getUserById);
-router.post("/users", createUser);
-router.put("/users/:id", updateUser);
-router.delete("/:id", deleteUser);
+
+export default router;
