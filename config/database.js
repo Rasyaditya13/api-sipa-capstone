1e7404b190f225d688189a1479f0c05af00b33a5
asus <rasyadelgiva@gmail.com>
2025-03-07 14:26:46 +0700

    Tes code

--- config/database.js
+++ config/database.js
@@ -1,13 +1,13 @@
-import {sequelize} from "sequelize";
+import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const db = new sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD,{
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
-    dialect: "mySql",
+    dialect: "mysql",
     logging: false,
 });
 
-export default sequelize;
 
+export default db;
