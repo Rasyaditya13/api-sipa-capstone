a23c2ab9fbcf94e9211e0627660c14dfed2b018f
asus <rasyadelgiva@gmail.com>
2025-03-08 13:31:41 +0700

    login regis

--- models/users.js
+++ models/users.js
@@ -11,15 +11,16 @@ const User = db.define("users", {
     nama: {
         type: DataTypes.STRING,
         allowNull: false,
-    },
-    email: {
-        type: DataTypes.STRING,
-        allowNull: false,
         unique: true,
     },
     password: {
         type: DataTypes.STRING,
         allowNull: false,
+    },
+    role: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        defaultValue: "tamu",
     },
 });
 
