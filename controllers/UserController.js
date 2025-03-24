1d0a698f3a7efd244975ac06e72ea48f49b0b5f9
asus <rasyadelgiva@gmail.com>
2025-03-24 12:48:41 +0700

    update login role

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -31,40 +31,28 @@ export const login = async (req, res) => {
 
     const user = await User.findOne({ where: { email } });
     if (!user) {
-      return res.status(401).json({ error: "Username atau password salah" });
+      return res.status(401).json({ error: "Email atau password salah" });
     }
 
     const isPasswordValid = await bcrypt.compare(password, user.password);
     if (!isPasswordValid) {
-      return res.status(401).json({ error: "Username atau password salah" });
+      return res.status(401).json({ error: "Email atau password salah" });
     }
 
-    const userRole = user.role;
-
-    const roleRedirect = {
-      tamu: "/beranda",
-      petugas: "/page-admin",
-    };
-    let redirectPage = roleRedirect[userRole] || "/";
-
-
-    const token = jwt.sign (
+    const token = jwt.sign(
       { id: user.id, email: user.email, role: user.role },
       process.env.JWT_SECRET,
       { expiresIn: "1h" }
     );
 
-    return res.status(200).json({
+    res.status(200).json({
       message: "Login berhasil",
       user: {
         id: user.id,
         email: user.email,
-        password: user.password,
-        token: token,
-        role: userRole,
+        role: user.role,
       },
       token: token,
-      redirectPage: redirectPage,
     });
   } catch (error) {
     return res.status(500).json({ error: error.message });
--- routes/middleware/middleware.js
+++ routes/middleware/middleware.js
@@ -1,24 +1,35 @@
 import jwt from "jsonwebtoken";
 
 export const verifyToken = (req, res, next) => {
-    const authHeader = req.headers['authorization'];
+  const authHeader = req.headers["authorization"];
 
-    if (!authHeader) {
-        return res.status(403).json({ message: 'Token required' });
-    }
+  if (!authHeader) {
+    return res.status(403).json({ message: "Token required" });
+  }
 
-    const token = authHeader.split(' ')[1];
-    if (!token) {
-        return res.status(401).json({ message: 'Token invalid' });
-    }
+  const token = authHeader.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({ message: "Token invalid" });
+  }
 
-    try {
-        const verifiedToken = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = verifiedToken;
-        req.user_id = verifiedToken.id;
-        next();
-    } catch (error) {
-        console.log(error);
-        return res.status(401).json({ message: 'Token invalid or expired' });
+  try {
+    const verifiedToken = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = verifiedToken;
+    req.user_id = verifiedToken.id;
+    next();
+  } catch (error) {
+    console.log(error);
+    return res.status(401).json({ message: "Token invalid or expired" });
+  }
+};
+
+export const authorize = (roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return res
+        .status(403)
+        .json({ message: "Access denied! Insufficient role." });
     }
-}
+    next();
+  };
+};
